49eb02919f2bd0af98cc1ece54c782c8b4bf05bd
L'x <qianfeng@server.local>
2014-10-17 17:35:40 +0800

    Initial Commit

---
+++ TestTenBlog/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestTenBlog
+//
+//  Created by L'x on 14-10-17.
+//  Copyright (c) 2014年 LittleX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
